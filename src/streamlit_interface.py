0d68db0e121a3c2396ecd8d3a8951db5f22905a5
Sambonic <124459402+Sambonic@users.noreply.github.com>
2024-06-01 23:45:57 +0300

    Update streamlit_interface.py

--- src/streamlit_interface.py
+++ src/streamlit_interface.py
@@ -19,19 +19,20 @@ with st.sidebar:
         if api_key:
             st.session_state["api_key"] = api_key
             st.write(establish_api(api_key))
+        st.write("**To get started, you'll need a Gemini API key:**")
+
+        instructions = """
+        1. **Go to Google AI Studio:** Login and access the platform.
+        2. **Click "Get API Key":** Locate the option to obtain an API key.
+        3. **Choose Project:** Select a new or existing project for the API key.
+        4. **Copy the Key:** Once generated, copy the API key here and have fun!
+        """
+
+        st.markdown(instructions)
     else:
-        st.write("API Key already entered. Results:")
+        st.write("API Key already entered. Enjoy :D")
     
-    st.write("**To get started, you'll need a Gemini API key:**")
-
-    instructions = """
-    1. **Go to Google AI Studio:** Login and access the platform.
-    2. **Click "Get API Key":** Locate the option to obtain an API key.
-    3. **Choose Project:** Select a new or existing project for the API key.
-    4. **Copy the Key:** Once generated, copy the API key here and have fun!
-    """
-
-    st.markdown(instructions)
+    
 
 
 # Send, recieve and preserve conversation history
