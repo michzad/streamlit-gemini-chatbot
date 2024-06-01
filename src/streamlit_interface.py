77622fc38a5869e6f0c65302547b0fe244972ef8
Sambonic <124459402+Sambonic@users.noreply.github.com>
2024-06-01 23:44:20 +0300

    Fixed API key problem using session states

--- src/streamlit_interface.py
+++ src/streamlit_interface.py
@@ -11,11 +11,27 @@ message_history = st.session_state['message_history']
 st.title("Hello, Streamlit!")
 st.write("This is my first attempt at creating a simple chatbot using gemini API and streamlit :D")
 
+# Insert API key to get started
 with st.sidebar:
-    st.title("Insert API Key to get started!")
-    key = st.text_input("")
-    if key:
-        st.write(establish_api(key))
+    if "api_key" not in st.session_state:
+        st.title("Insert API Key!")
+        api_key = st.text_input("")
+        if api_key:
+            st.session_state["api_key"] = api_key
+            st.write(establish_api(api_key))
+    else:
+        st.write("API Key already entered. Results:")
+    
+    st.write("**To get started, you'll need a Gemini API key:**")
+
+    instructions = """
+    1. **Go to Google AI Studio:** Login and access the platform.
+    2. **Click "Get API Key":** Locate the option to obtain an API key.
+    3. **Choose Project:** Select a new or existing project for the API key.
+    4. **Copy the Key:** Once generated, copy the API key here and have fun!
+    """
+
+    st.markdown(instructions)
 
 
 # Send, recieve and preserve conversation history
